684fbc03030cc3dc0f2b4a3342a58ca830917be6
sonia <33809381+sonia83@users.noreply.github.com>
2017-11-25 18:50:07 +0100

    Ajout d'annotation

--- sha1.cpp
+++ sha1.cpp
@@ -1,20 +1,21 @@
-#include "openssl/sha.h"
+#include "openssl/sha.h"  //openssl est la biblio contenant la fonction de hachage "sha1"
 #include <string.h>
 #include <stdio.h>
 
 int main(){
 	// The data to be hashed
-	const unsigned char data[] = "Notre phrase hashée!!";
+	const unsigned char data[] = "Notre phrase hashée!!";  //data = la phrase a hasher
 	size_t length = sizeof(data);
-	unsigned char hash[SHA_DIGEST_LENGTH];
-        
-	SHA1(data, length, hash);
+	unsigned char hash[SHA_DIGEST_LENGTH];		 	// hash = ??
+								//SHA_DIGEST_LENGTH == 20 octets de sortie 
+	
+	SHA1(data, length, hash);  			// sha1 = hachage du "data" de longueur "length" sur 160 bits 
 
-	char mdString[SHA_DIGEST_LENGTH];
+	char mdString[SHA_DIGEST_LENGTH]; 		// ??
 	 
-	for(int i = 0; i < (SHA_DIGEST_LENGTH/2) - 1; i++)
+	/* ---- Affichage de la phrase hashé ----- */
+	for(int i = 0; i < (SHA_DIGEST_LENGTH/2) - 1; i++)      
            sprintf(&mdString[i*2], "%02x", (unsigned int)hash[i]);
- 
     	printf("sha1 digest: %s\n", mdString);
 
 	return 0;
